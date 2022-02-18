bcde5b9a6a1623b9c15e77d85d7324211ac370ce
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-18 16:35:16 -0300

    Req 8-adiciona Title ao Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        Teste
+        <Title headline="Missões" />
       </div>
     );
   }
