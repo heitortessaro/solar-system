4c4fef66768d161affb9396a1380ea145b8e7ce2
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-18 16:58:39 -0300

    fix lint problem

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Title from './Title';
-import missions from '../data/missions'
+import missions from '../data/missions';
 import MissionCard from './MissionCard';
 
 class Missions extends Component {
