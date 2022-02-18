a9c68212d948a29631d335e78f9fecea83cd3c63
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-18 16:05:37 -0300

    Req 5 - fix PropTypes problems PlanetCard

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
-      // <div data-testid="solar-system">Teste</div>
-      <Title headline="Planetas" />
+      <section data-testid="solar-system">
+        <Title headline="Planetas" />
+        <PlanetCard planetName={ planets[0].name } planetImage={ planets[0].image } />
+      </section>
     );
   }
 }
