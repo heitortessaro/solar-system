8cc02f699ee17e49722900d7568eaecb7701b20d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-02-18 15:09:44 -0300

    Req 1-adiciona header ao app. Finaliza req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar Teste</p>);
+    return (
+      <div className="App">
+        <Header />
+        <p>Sistema Solar Teste</p>
+      </div>
+    );
   }
 }
 
